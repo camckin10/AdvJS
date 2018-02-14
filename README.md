5ec10a3a049b76018246e6b12c05d63fd266160e
camckin10 <chelseymckinney2@gmail.com>
2018-02-13 18:19:02 -0600

    updating assignment

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AdvJS
 Advanced JS Assignment
+
+Assignment continuing practice on Node.js. We created a Hangman game through the back end.
+
+Technologies used: JavaScript and Node.js
--- index.js
+++ index.js
@@ -8,7 +8,6 @@
 var inquire = inquirer('./letter.js');
 var secondInquire = inquirer('./word.js');
 
-
 //questions for game
 //inquirer.prompt
 //ask user --guess a letter
@@ -16,41 +15,78 @@ var secondInquire = inquirer('./word.js');
 //cities/planets of superheroes
 //wakanda,gotham,tamaran,mogo, krypton, mars
 
-//best option to set up like answer object?
-inquirer.prompt([
-  {
-    //question 1
-    type: list,
-    message: "home planet of Black Panther",
-    name: "wakanda" //7 lines or characters
-  }, {
-    //question 2
-    type : list,
-    message: "city where Batman is from",
-    name : "gotham" //6 lines or characters
-  }, {
-    //question 3
-    type:list,
-    message:"home planet of starfire",
-    name: "tamaran" //7 lines or characters
-  }, {
-    //question 4
-    type:list,
-    message:"home planets of all Green Lanterns",
-    name: "mogo" //4 lines or characters
-  }, {
-    //question 5
-    type: list,
-    message: "home planet of superman",
-    name: "krypton" //7 lines or characters
-  }, {
-    //question 6
-    type: list,
-    message:"planet in the real universe and superhero universe",
-    name: "mars" //4 lines or 4 characters
+//cities/planets of superheroes
+//wakanda,gotham,tamaran,mogo, krypton, mars
+
+Programmer.prototype.printInfo = function() {
+  console.log
+  ("Name: " + this.name + "\nPosition: " + this.position +
+  "\nAge: " + this.age + "\nLanguages: " + this.language);
+
+  console.log("---------------");
+};
+
+// variable we will use to count how many times our questions have been asked
+var count = 0;
+// array in which we will store each of our new programmer objects
+var answerArray = [];
+
+var askQuestion = function() {
+  // if statement to ensure that our questions are only asked five times
+  if (count < 6) {
+    console.log("This is a test");
+    // runs inquirer and asks the user a series of questions whose replies are
+    // stored within the variable answers inside of the .then statement
+    inquirer.prompt([
+      {
+        name: "one",
+        message: "What planet is Superman from?" //krypton
+      }, {
+        name: "two",
+        message: "What planet are all Green Lanterns from?" //mogo
+      }, {
+        name: "three",
+        message: "City where Batman is from?" //gotham
+      }, {
+        name: "four",
+        message: "Planet in the real universe and comic book universe?" //mars
+      }, {
+        name: "five",
+        message: "Planet where starfire is from?" //tamaran
+      }, {
+        name: "six",
+        message: "Planet of Black Panther?" //wakanda
+      }
+    ]).then(function(answers) {
+
+      // pushes newguy object into our array
+      answerArray.push(new Question);
+      // add one to count to increment our recursive loop by one
+      count++;
+      // run the askquestion function again so as to either end the loop or ask the questions again
+      askQuestion();
+    });
+    // else statement which runs a for loop that will execute .printInfo() for each object inside of our array
   }
+  else {
+    for (var x = 0; x < answerArray.length; x++) {
+      //print error message
+      //answerArray[x].();
+    }
+  }
+};
 
-])
+// call askQuestion to run our code
+askQuestion();
 
-//create a for loop to loop through empty spaces?
-//print empty lines are strings?
+
+//function with a for loop:
+//figure out length of string (for answers)
+//print the amount of placeholders '_'
+// var gameAnswers = ['gotham', 'mars', 'krypton'];
+// function gameQuestions () {
+//   var gameAnswers = ['gotham', 'mars', 'krypton'];
+//   for(var i = 0; i < gameAnswers.length; i ++) {
+//     console.log('_');
+//   }
+// }
